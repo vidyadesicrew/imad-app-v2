923105902175844aa34988b218cb6fb159a45aae
vidyadesicrew <vidyashree@desicrew.in>
2017-02-18 14:19:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+    res.send("Article one requested & will be servered here");
+});
+
+app.get('/article-two', function(req, res){
+    res.send("Article two requested & will be servered here");
+});
+
+app.get('/article-three', function(req, res){
+    res.send("Article three requested & will be servered here");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
