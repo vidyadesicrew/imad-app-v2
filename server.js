df0d5a8c81df268206a3dc2a7af8e83da1056f0e
vidyadesicrew <vidyashree@desicrew.in>
2017-02-22 12:36:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,83 +5,90 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles ={
-'article-one'={
-    title: 'Article One - Vidya',
-    heading: 'Article One',
-    date: 'Feb 18th, 2017',
-    content:  'This is the content my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.'
-  
+var articles = {
+ 'article-one' : {
+title: 'Article One / chethana',
+heading: 'Article One',
+date: 'February 16th, 2017',
+content:     
+                <p>
+                    Spear fisherman Danny Henricks is fortunate enough to have survived the type of underwater experience that proves just how dangerous sea creatures can be -- and he even caught it all on video.
+                </p> '
 },
 
-'article-two'={
-    title: "Article Two - Vidya",
-    heading: "Article Two",
-    date: "Feb 18th, 2017",
-    content:                   <p>
-                        This is the content for my second article.
-                    </p>
-    },
-    
-'article-three'={
-    title: "Article Three - Vidya",
-    heading: "Article Three",
-    date: "Feb 18th, 2017",
-    content:                   <p>
-                        This is the content for my third article.
-                    </p>
-}
-};
-function createtemplate (data){
+ 'article-two' : {
+     title: 'Article Two / chethana',
+heading: 'Article Two',
+date: 'February 16th, 2017',
+content:     
+                <p>
+                    Spear fisherman Danny Henricks is fortunate enough to have survived the type of underwater experience that proves just how dangerous sea creatures can be -- and he even caught it all on video.
+                </p> '
+ },
+ 
+ 'article-three' : {
+     title: 'Article Three / chethana',
+heading: 'Article Three',
+date: 'February 20th, 2017',
+content:     
+                <p>
+                    Spear fisherman Danny Henricks is fortunate enough to have survived the type of underwater experience that proves just how dangerous sea creatures can be -- and he even caught it all on video.
+                    Henricks, 35, was fishing off the coast of Australia when a bull shark charged at him at full speed, only to impale himself on Danny's spear gun.
+                    The hair-raising clip has been video more than three million times (and counting) since Henricks uploaded it last month.
+                </p> '
+ };
+ 
+
+function createtemplate (data) {
     var title = data.title;
-    var heading = data.heading;
     var date = data.date;
+    var heading = data.heading;
     var content = data.content;
     
-var htmltemplate='<html>
+var htmltemplate = 
+<html>
     <head>
         <title>
             ${title}
         </title>
-            <meta name="viewport" content="width=device-width, initial-sacle-1"/>
-            <link href="/ui/style.css" rel="stylesheet" />
-    </head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
         <body>
-            <div class ="container">
-                <div>
-                    <a href ="/">Home</a>
-                </div>
-                <hr/>
-                <h3>${heading}</h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                    ${content}
+            <div class="container">
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
                 </div>
             </div>
         </body>
-</html>'
-;
-return htmltemplate;
+    </head>
+</html>
+   ;
+   return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articlename', function(req, res){
-    // articlename == article one
-    //article[articlename]=={} content object for article one
-    var articleName = req.params.articlename;
-    res.send(createtemplate(articles[articlename]));
+app.get('/article-one', function (req, res) {
+    //articlename == article-one
+    //articles[articlename] == {} content object for the article one
+    var articlename = req.params.articlename;
+   res.send(createTemplate(articles[articlename]));
 });
 
-app.get('/ui/style.css', function (req, res) {
+    app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
