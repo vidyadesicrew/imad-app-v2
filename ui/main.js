331b9c6d6ce2764dada245913e8847d9b6b6ea5e
vidyadesicrew <vidyashree@desicrew.in>
2017-02-23 16:07:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 //Change the text of the main-text div
-var element = document.getElementById("'main-text");
+var element = document.getElementById("main-text");
 
 element.innerHTML = 'New Value';
 
