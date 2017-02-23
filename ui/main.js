01cb4b571f46b37b221b6822ee677100ebd68593
vidyadesicrew <vidyashree@desicrew.in>
2017-02-23 16:07:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 console.log('Loaded!');
 
 //Change the text of the main-text div
-var element = document.getElementById('main-text');
+var element = document.getElementById("'main-text");
 
 element.innerHTML = 'New Value';
 
 //Move the image
-var imag = document.getElementById('madi');
+var imag = document.getElementById("madi");
 function moveRight() {
     marginLeft = marginLeft+10;
     img.style.marignLeft = marginLeft + 'px';
