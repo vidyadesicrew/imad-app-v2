4d01bb57ac50f4938405ad94dedbf5b761f13e19
vidyadesicrew <vidyashree@desicrew.in>
2017-02-23 16:13:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
 
 //Change the text of the main-text div
-var element = document.getElementById("main-text");
+var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 
 //Move the image
-var imag = document.getElementById("madi");
+var imag = document.getElementById('madi');
+marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft+10;
+    marginLeft = marginLeft + 1;
     img.style.marignLeft = marginLeft + 'px';
-    var marginLeft = 0;
-}
+   }
 imag.oneclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
    };
