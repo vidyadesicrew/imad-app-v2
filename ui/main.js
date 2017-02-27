be1bd3a6908c0b1f551112d4c9ce890d3cb82e8b
vidyadesicrew <vidyashree@desicrew.in>
2017-02-27 16:42:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,25 @@
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function(){
    
-   // Make a request to the counter endpoint
+   // Create a request object
+   var request = new XMLHttpRequest();
+   
    
    // Capture the response and store it in a variable
+   request.onreadystate = function() {
+       if (request.readyState === XMLHttpRequest.Done){
+           // Take some action
+           if (request.status === 200) {
+               var counter = request.responseText;
+           }
+       }
+       // Not done yet
+   };
    
-  // Render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-    
+  
+    // Make the request
+    request.open('GET', 'http://vidyadesicrew.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
