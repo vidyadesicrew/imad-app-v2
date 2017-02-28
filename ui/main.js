a5af06f73763b9bc9e6a9cd0ed17eee4cfa56a84
vidyadesicrew <vidyashree@desicrew.in>
2017-02-28 15:44:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,7 @@ button.onclick = function(){
    // Create a request object
    var request = new XMLHttpRequest();
    
-   
-   // Capture the response and store it in a variable
+      // Capture the response and store it in a variable
    request.onreadystatechange = function() {
        if (request.readyState === XMLHttpRequest.DONE){
            // Take some action
@@ -20,8 +19,24 @@ button.onclick = function(){
        // Not done yet
    };
    
-  
-    // Make the request
+      // Make the request
     request.open('GET', 'http://vidyadesicrew.imad.hasura-app.io/counter', true);
     request.send(null);
+};
+
+// Subit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    // Make a request to the server and send the name
+    
+    // Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i<names.length; i++){
+        list += '<li>'+ names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
