3c60ab55578c62a47f95f496cc74a1014380b54b
vidyadesicrew <vidyashree@desicrew.in>
2017-03-09 15:50:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
 
+var config = {
+    user: 'vidyadesicrew',
+    database: 'vidyadesicrew',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -79,6 +87,19 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    // Make a seleect request
+    // Return a response with the result
+    pool.query('SELECT * FROM test', function(err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter = 0;
