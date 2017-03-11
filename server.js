1e9025fbe2146ad2e2851a17d0d3f2eef00ef999
vidyadesicrew <vidyashree@desicrew.in>
2017-03-11 17:18:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'vidyadesicrew',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'process.env.DB_PASSWORD'
+    password: 'Vidya@123'
 };
 var app = express();
 app.use(morgan('combined'));
