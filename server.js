01805789e47d47b617773e96184fa9613d50cf6a
vidyadesicrew <vidyashree@desicrew.in>
2017-03-11 17:01:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/test-db', function(req, res) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            res.send(JSON.stringify(result, rows));
+            res.send(JSON.stringify(result.rows));
         }
         
     });
