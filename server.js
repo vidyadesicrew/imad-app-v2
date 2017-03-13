afcf73b4ce1dd804e9a38ef66b24de3bf09f7a14
vidyadesicrew <vidyashree@desicrew.in>
2017-03-13 15:12:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,38 +14,6 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articles = {
-'article-one':  {
-    title: 'Article one - vidya',
-    heading:'Article one',
-    date: 'Feb 18, 2017',
-    content: ` <p> 
-            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p> 
-             <p> 
-            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-             <p> 
-            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>`
-},
-'article-two': {
-    title: 'Article Two - vidya',
-    heading:'Article Two',
-    date: 'Feb 18, 2017',
-    content: ` <p> 
-            This is the content for my second article.
-            </p>`
-},
-'article-three': {
-    title: 'Article Three - vidya',
-    heading:'Article Three',
-    date: 'Feb 18, 2017',
-    content: ` <p> 
-            This is the content for my third article.
-            </p>`
-}
-};
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
