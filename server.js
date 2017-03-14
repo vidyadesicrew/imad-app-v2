d7f1e3726f123c41618d4aa926f313d205c6a3ce
vidyadesicrew <vidyashree@desicrew.in>
2017-03-14 16:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/test-db', function(req, res) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            res.send(JSON.stringify(result.rows));
+            res.send('User successfully created: ' +username);
         }
         
     });
