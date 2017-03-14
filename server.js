1f89b1cc36783c03bdea75cbb639f62c12091f00
vidyadesicrew <vidyashree@desicrew.in>
2017-03-14 16:23:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'vidyadesicrew',
@@ -13,7 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
