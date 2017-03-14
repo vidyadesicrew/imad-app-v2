3f483b6304bcaecb5ec1eb77332828e5a2a4d308
vidyadesicrew <vidyashree@desicrew.in>
2017-03-14 17:14:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,10 +78,11 @@ app.get('/hash/:input', function(req, res) {
     
 app.post('/create-user', function (req, res) {
     // username, password
+    // "username" : "vidyadesicrew" "password" : "password"
     // JSON
     var username = req.body.username;
     var password = req.body.password;
-    var salt = crypto.getRandomBytes(128).toString('hex');
+    var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     pool.query('INSERT INTO "user" (username, password) values($1, $2)', [username, dbString], function (req, res) {
         if (err) {
