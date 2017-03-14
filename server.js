9cafa51789baab78be9c6ec88536634479d69898
vidyadesicrew <vidyashree@desicrew.in>
2017-03-14 16:19:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,24 @@ app.get('/hash/:input', function(req, res) {
     res.send(hashedString);
     
 });
+    
+app.post('/create-user', function (req, res) {
+    // username, password
+    // JSON
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) values($1, $2)', [username, dbString], function (req, res) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result.rows));
+        }
+        
+    });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
     // Make a select request
