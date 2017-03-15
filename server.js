820c53b3a58c9e17426e0727c6897c1fe02608fc
vidyadesicrew <vidyashree@desicrew.in>
2017-03-15 13:50:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,17 +78,45 @@ app.get('/hash/:input', function(req, res) {
     
 app.post('/create-user', function (req, res) {
     // username, password
-    // "username" : "vidyadesicrew" "password" : "password"
+    // { "username" : "vidya", "password" : "password"}
     // JSON
     var username = req.body.username;
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user" (username, password) values($1, $2)', [username, dbString], function (req, res) {
+    pool.query('INSERT INTO "user" (username, password) values($1, $2)', [username, dbString], function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
             res.send('User successfully created: ' + username);
+        }
+        
+    });
+});
+
+app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
+     pool.query('SELECT * from "user" username = $1', [username], function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length ===0){
+                res.send(403).send('username/password is invalid');
+            } else {
+                // Match the password
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedpassword = has(password, salt); // Creating a hash based on the password submitted and the original salt
+                if(hashedPassword ===dbString) {
+                res.send('credentials correct');
+                } else {
+                     res.send(403).send('username/password is invalid');
+                }
+                
+            }
+            
         }
         
     });
