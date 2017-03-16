52cf5fc896d7fe0aaed863cbe2f3cc0eeccb01ef
vidyadesicrew <vidyashree@desicrew.in>
2017-03-16 11:52:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.get('/', function (req, res) {
 function hash (input, salt) {
     // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512,'sha512');
+    console.log(" hashed password ="+hashedPassword);
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
     
     // algorithm: md5
