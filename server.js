eadc6fe3a54b68badace515c7f8bbe483d01b089
vidyadesicrew <vidyashree@desicrew.in>
2017-03-16 11:57:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function hash (input, salt) {
 
 app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
-        res.send(hashedString);
+    res.send(hashedString);
     
 });
     
