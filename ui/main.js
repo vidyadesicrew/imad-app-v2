cee0881a08ee2d51d29f4e3f33c2e53a109d8cc2
vidyadesicrew <vidyashree@desicrew.in>
2017-03-21 17:03:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,10 +10,11 @@ submit.onclick = function() {
        if (request.readyState === XMLHttpRequest.DONE){
            // Take some action
           if(request.status===200){
+              console.log('user logged in');
               alert('Logged in successfully');
-          } else if (request.status=== 403){
-              alert('Username/password is incorrect');
-          } else if (request.status ===500){
+          } else if (request.status === 403) {
+              alert('Usename/password in incorrect');
+          } else if (request.status === 500){
               alert('Something went wrong on the server');
           }
         
