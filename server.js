77f8fec42e168e011d565d7aaae45614004fc7ca
vidyadesicrew <vidyashree@desicrew.in>
2017-03-21 17:29:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'vidyadesicrew',
@@ -15,6 +16,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: {maxAge: 1000 * 60 * 24 * 30}
+}));
 
 var pool = new Pool(config);
 function createTemplate (data) {
@@ -111,7 +116,14 @@ app.post('/login', function (req, res) {
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
                 if(hashedPassword === dbString) {
+                    
+                    // Set the session
+                    req.seesion.auth = {userID: result.rows[0].id};
+                    // set cookie with a session id
+                    // internally, on the server side, it maps the session id to an object
+                    // { auth:
                 res.send('credentials correct!');
+                
                 } else {
                      res.status(403).send('username/password is invalid');
                 }
