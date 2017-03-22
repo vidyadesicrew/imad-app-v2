fcce697385e19fd9654d9550c0bb170f61f43208
vidyadesicrew <vidyashree@desicrew.in>
2017-03-22 17:01:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -199,6 +199,14 @@ app.get('/articles/:articleName', function (req, res) {
       
 });
 
+app.get('/:articleName', function (req, res) {
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));    
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
