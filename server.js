97ff5466d05ff3948d73d1d73de2886403eb96cf
vidyadesicrew <vidyashree@desicrew.in>
2017-03-22 17:05:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -206,6 +206,25 @@ app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[articleName]));    
 });
 
+app.get('/articles/:articleName', function (req, res) {
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    
+    // SELECT * FROM article where title = 'article-one'
+      pool.query("SELECT * FROM article where title = '" + req.params.articleName + "'", function (err, result) {
+          if (err) {
+              res.status(500).send(err.toString());
+          } else {
+              if (result.rows.length === 0) {
+                  res.status(404).send('Article Not Found');
+              } else {
+                  var articleData = result.rows[0];
+                   res.send(createTemplate(articleData)); 
+              }
+          }
+          
+      });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
