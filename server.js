33ee9ea06f011763d09aae316155ae8bab2eca12
vidyadesicrew <vidyashree@desicrew.in>
2017-03-29 16:34:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,17 +133,25 @@ app.post('/login', function (req, res) {
 });
 
 app.get('/check-login', function (req, res) {
-    if(req.session && req.session.auth && req.session.auth.userId) {
-        res.send('You are logged in: ' + req.session.auth.userId.toString());
-    } else { 
-        res.send('You are not logged in');
-    }
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 
 app.get('/logout', function (req, res) {
-    delete req.session.auth;
-    res.send('Logged out');
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
+
 
 app.get('/test-db', function(req, res) {
     // Make a select request
